ff36e20fc47192d9e545170ead16d25d8478989d
Manan Shah <2400090137@kluniversity.in>
2026-05-01 20:11:28 +0530

    Update HomeController.java

--- src/main/java/com/artvista/backend/controller/HomeController.java
+++ src/main/java/com/artvista/backend/controller/HomeController.java
@@ -3,7 +3,8 @@ package com.artvista.backend.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin(origins = "*")
 @RestController
 public class HomeController {
 
